404cef85700a0c92e9277b235b42b3f61e48852f
Bilel MOSBAH <bmosbah.ext@galerieslafayette.com>
2024-11-29 20:26:15 +0100

    fix github-actions-provider

--- iam-workload-identity-federation.tf
+++ iam-workload-identity-federation.tf
@@ -7,8 +7,11 @@ resource "google_iam_workload_identity_pool_provider" "github-actions-provider" 
   workload_identity_pool_id          = google_iam_workload_identity_pool.github-actions-pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-actions-provider"
   attribute_mapping                  = {
-    "google.subject"       = "assertion.repository_owner" 
+    "google.subject"       = "assertion.sub"
   }
+  attribute_condition = <<EOT
+    attribute.repository == "Luap989/Detection-de-fraudes"
+EOT
   oidc {
     issuer_uri        = "https://token.actions.githubusercontent.com/"
   }
--- iam.tf
+++ iam.tf
@@ -1,7 +1,7 @@
 resource "google_service_account_iam_member" "github-action-SA-workloadIdentityUser" {
   service_account_id = google_service_account.github_action_deploy.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principal://iam.googleapis.com/projects/497650107059/locations/global/workloadIdentityPools/github-actions-pool/subject/Pillouhabsom"
+  member             = "principal://iam.googleapis.com/projects/497650107059/locations/global/workloadIdentityPools/github-actions-pool/subject/Luap989"
 }
 
 resource "google_project_iam_member" "github-actions-project-owner" {
