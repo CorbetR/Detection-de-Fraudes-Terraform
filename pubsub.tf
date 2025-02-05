43bc59f3c4ce3350316e2c78bf701690f92ba958
VotreNomUtilisateur <votre@email.com>
2025-02-05 17:27:59 +0100

    modif pub sub

--- pubsub.tf
+++ pubsub.tf
@@ -1,8 +1,15 @@
-resource "google_pubsub_subscription" "gcs_topic_push_subscription" {
-  name  = "gcs-event-subscription"  # Choisis le nom de ton choix
-  topic = google_pubsub_topic.gcs_topic.name
+resource "google_pubsub_subscription" "push_subscription" {
+  name  = "push-to-cloud-run"
+  topic = google_pubsub_topic.gcs_topic.id
 
-#  push_config {
-#    push_endpoint = "https://mon-application-497650107059.us-central1.run.app"
-#  }
+  # Configuration pour envoyer les messages à Cloud Run
+  push_config {
+    push_endpoint = "https://cloud-run-test-497650107059.us-central1.run.app" 
+
+    # Authentification via un Service Account
+    oidc_token {
+      service_account_email = google_service_account.github_action_deploy.email
+    }
+  }
 }
+
