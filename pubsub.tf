2a3d32608084367c41cacafee5f15d46c8ddd24c
VotreNomUtilisateur <votre@email.com>
2025-02-05 15:33:36 +0100

    création pub/sub

--- pubsub.tf
+++ pubsub.tf
@@ -2,7 +2,7 @@ resource "google_pubsub_subscription" "gcs_topic_push_subscription" {
   name  = "gcs-event-subscription"  # Choisis le nom de ton choix
   topic = google_pubsub_topic.gcs_topic.name
 
-  push_config {
-    push_endpoint = "https://mon-application-497650107059.us-central1.run.app"
-  }
+#  push_config {
+#    push_endpoint = "https://mon-application-497650107059.us-central1.run.app"
+#  }
 }
